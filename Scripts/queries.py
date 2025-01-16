1e01b1797c524d0956f31a14f8af1b60553d5b5b
brendes6 <brendes6@gmail.com>
2025-01-16 15:45:59 -0500

    Added visualizations and analysis

--- Scripts/queries.py
+++ Scripts/queries.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import mysql.connector as sql
 
+# Fill in with your mysql user/password
+my_user = ""
+my_password = ""
+
 def query_data(query):
-    connection = sql.connect(host="localhost", user="root", password="Daisydog1200$", 
+    connection = sql.connect(host="localhost", user=my_user, password=my_password, 
                          database="ufc_matchup_database", connection_timeout=300)
     df = pd.read_sql(query, connection)
     connection.close()
